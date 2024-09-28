9a7a0bc753cb2dd1d1f1613433e283daa150a535
alidotj <ali.joodie.s@gmail.com>
2024-09-28 19:11:22 +0330

    57

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,21 +1,17 @@
 import { Component, input } from '@angular/core';
 import { ITask } from '../Models/itask';
-
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
 export class TaskComponent {
   task = input.required<ITask>();
 
-
-
   onCompleteTask() {
     this.task().completed = !this.task().completed;
   }
-
-  
 }
