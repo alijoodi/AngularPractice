b78b9ea8c9940d0d05c4bd45a305291f41d798ef
alidotj <ali.joodie.s@gmail.com>
2024-09-28 18:50:48 +0330

    56

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,11 +10,12 @@ import {
 } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 import { IUser } from '../Models/iuser';
+import { CardComponent } from "../shared/card/card.component";
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
