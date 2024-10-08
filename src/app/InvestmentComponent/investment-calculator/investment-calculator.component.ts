98ef11a58ee89146c4bfa853070f459ba7295492
alidotj <ali.joodie.s@gmail.com>
2024-10-08 17:44:54 +0330

    84

--- src/app/InvestmentComponent/investment-calculator/investment-calculator.component.ts
+++ src/app/InvestmentComponent/investment-calculator/investment-calculator.component.ts
@@ -1,6 +1,7 @@
 import {
   Component,
   EventEmitter,
+  NgModule,
   Output,
   output,
   Signal,
@@ -10,27 +11,25 @@ import { FormsModule } from '@angular/forms';
 import { type IInvestmentinput } from '../../Models/iinvestmentinput';
 import { InvestmentCalculatorService } from '../../Services/investment-calculator.service';
 import { AnnualInvestmentData } from '../../Models/annual-investment-data';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 @Component({
   selector: 'app-investment-calculator',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CurrencyPipe,DatePipe],
   templateUrl: './investment-calculator.component.html',
   styleUrl: './investment-calculator.component.css',
 })
 export class InvestmentCalculatorComponent {
-  /**
-   *
-   */
 
   @Output() investValueOutput = new EventEmitter<IInvestmentinput>();
   constructor(
     private investmentCalculatorService: InvestmentCalculatorService
   ) {}
   investValue: IInvestmentinput = {
-    InitialInvestment: 0,
-    ExpectedReturn: 0,
-    Duration: 0,
-    AnnualInvestment: 0,
+    InitialInvestment: 1000,
+    ExpectedReturn: 1,
+    Duration: 10,
+    AnnualInvestment: 1,
   };
 
   ReturnValue() {
--- src/app/InvestmentComponent/investment-result/investment-result.component.ts
+++ src/app/InvestmentComponent/investment-result/investment-result.component.ts
@@ -1,18 +1,20 @@
-import { Component, input } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 import { InvestmentCalculatorService } from '../../Services/investment-calculator.service';
 import { IInvestmentinput } from '../../Models/iinvestmentinput';
 import { AnnualInvestmentData } from '../../Models/annual-investment-data';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-investment-result',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-result.component.html',
   styleUrl: './investment-result.component.css',
 })
 export class InvestmentResultComponent {
   investValue = input<IInvestmentinput>();
   calculatedResultInput = input<AnnualInvestmentData[]>();
+  @Input() calculatedResultInput1: AnnualInvestmentData[] = [];
   calculatedResult: AnnualInvestmentData[] = [];
   constructor(
     private investmentCalculatorService: InvestmentCalculatorService
