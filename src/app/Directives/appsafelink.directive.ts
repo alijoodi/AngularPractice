107d94be6a0cc08be821d06d4cef943230543f78
alidotj <ali.joodie.s@gmail.com>
2024-10-21 12:34:54 +0330

    155

--- src/app/Directives/appsafelink.directive.ts
+++ src/app/Directives/appsafelink.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, inject } from '@angular/core';
+import { Directive, ElementRef, Inject, inject, input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MessageboxComponent } from '../messagebox/messagebox.component';
 @Directive({
@@ -9,13 +9,18 @@ import { MessageboxComponent } from '../messagebox/messagebox.component';
   },
 })
 export class SafeLinkDirective {
+  private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
   readonly dialog = inject(MatDialog);
+  queryParam = input();
 
   constructor() {
     console.log('appSafeLink');
   }
 
   onClick(event: MouseEvent) {
+    const address = this.hostElementRef.nativeElement.href;
+    this.hostElementRef.nativeElement.href =
+      address + '?from=' + this.queryParam();
     this.dialog
       .open(MessageboxComponent, {
         data: {
@@ -27,7 +32,6 @@ export class SafeLinkDirective {
       .subscribe((data) => {
         console.log(data);
       });
-
 
     // const wantsToLeave = window.confirm(
     //   'Are you sure you want to leave this app?'
