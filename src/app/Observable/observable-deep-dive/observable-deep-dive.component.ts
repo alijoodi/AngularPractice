51c83713d6d7d5c71a795236f327dc0ae3d0757b
alidotj <ali.joodie.s@gmail.com>
2024-11-11 10:30:15 +0330

    208

--- src/app/Observable/observable-deep-dive/observable-deep-dive.component.ts
+++ src/app/Observable/observable-deep-dive/observable-deep-dive.component.ts
@@ -1,5 +1,12 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import {
+  Component,
+  DestroyRef,
+  effect,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-observable-deep-dive',
@@ -9,16 +16,34 @@ import { interval } from 'rxjs';
   styleUrl: './observable-deep-dive.component.css',
 })
 export class ObservableDeepDiveComponent implements OnInit {
+  clickCount = signal(0);
+  
+  constructor() {
+    effect(() => {
+      console.log(`Button clicked ${this.clickCount()} times`);
+    });
+  }
+
+  clickButton() {
+    this.clickCount.update((prevValue) => prevValue + 1);
+  }
+
+
   private destroyRef = inject(DestroyRef);
   ngOnInit(): void {
-    const subscaription = interval(1000).subscribe({
-      next(value) {
-        console.log(value);
-      },
-      complete() {
-        console.log('completeObservableDeepDive');
-      },
-    });
+    const subscaription = interval(1000)
+      .pipe(
+        map((val) => val + 3),
+        map((val) => val * 2)
+      )
+      .subscribe({
+        next(value) {
+          console.log(value);
+        },
+        complete() {
+          console.log('completeObservableDeepDive');
+        },
+      });
 
     this.destroyRef.onDestroy(() => {
       subscaription.unsubscribe();
