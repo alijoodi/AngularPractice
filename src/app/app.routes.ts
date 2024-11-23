eb9662fd0a47a095dca50a134eb93f18e0b6a678
alidotj <ali.joodie.s@gmail.com>
2024-11-23 14:48:23 +0330

    265

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from './UserComponent/task-list/task-list.component';
+import { ObservableDeepDiveComponent } from './Observable/observable-deep-dive/observable-deep-dive.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'observable', component: ObservableDeepDiveComponent }];
