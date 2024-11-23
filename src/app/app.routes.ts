b215175ab88388868402c880d194f30343f8ee31
alidotj <ali.joodie.s@gmail.com>
2024-11-23 14:52:33 +0330

    266

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,13 @@
 import { Routes } from '@angular/router';
 import { TaskListComponent } from './UserComponent/task-list/task-list.component';
 import { ObservableDeepDiveComponent } from './Observable/observable-deep-dive/observable-deep-dive.component';
+import { UserInputDeepDiveLoginComponent } from './UserInputDeepDive/user-input-deep-dive-login/user-input-deep-dive-login.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [{ path: 'observable', component: ObservableDeepDiveComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'observable',
+    component: ObservableDeepDiveComponent,
+  },
+];
